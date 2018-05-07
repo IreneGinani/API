ba7e41ae40a7d8cf1941b4ab8d01d97ed52c05bd
ireneginani <ireneginani@gmail.com>
2018-05-07 14:52:58 -0300

    primeiros passos do tutorial

---
+++ api/resources.py
@@ -0,0 +1,9 @@
+# api/resources.py
+
+from tastypie.resources import ModelResource
+from api.models import Note
+
+class NoteResource(ModelResource):
+    class Meta:
+        queryset = Note.objects.all()
+        resource_name = 'note'
