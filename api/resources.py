f90e655fed379c0b26e9630e8523bd7b82f33cdc
ireneginani <ireneginani@gmail.com>
2018-05-07 16:11:10 -0300

    adicionando método post

--- api/resources.py
+++ api/resources.py
@@ -1,9 +1,10 @@
-# api/resources.py
-
 from tastypie.resources import ModelResource
 from api.models import Note
+from tastypie.authorization import Authorization
 
 class NoteResource(ModelResource):
     class Meta:
         queryset = Note.objects.all()
         resource_name = 'note'
+        authorization = Authorization()
+        fields = ['title', 'body']
